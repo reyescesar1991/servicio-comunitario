ef6d3cd1b9b6f4ab3fc2cbcdd62af4fc17ba8ff1
sedett <sedetcontrerasc@gmail.com>
2025-03-01 10:27:44 -0400

    Prototipe home Page ( NO FUNCIONAL)

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -4,10 +4,12 @@ import { NavBarComponent } from '../../../shared/nav-bar/nav-bar.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { HeroComponent } from './hero/hero.component';
 import { FeaturesComponent } from './features/features.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, ButtonComponent, HeroComponent,FeaturesComponent],
+  imports: [RouterOutlet, NavBarComponent, ButtonComponent, HeroComponent,FeaturesComponent, CoursesComponent,FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
